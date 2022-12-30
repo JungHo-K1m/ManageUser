b5abf3547b483053ffcffafd006b2605b3a139c7
JungHo-K1m <qopqop55@naver.com>
2022-12-30 11:19:58 +0900

    PDF 병합하기

---
+++ JAVA/MERGE_PDFs/src/test/java/com/example/merge_pdfs/MergePdFsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.merge_pdfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergePdFsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
